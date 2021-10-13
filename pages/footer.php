01fca45ea582ec0c4b33eded769f17d60b46646d
SnakeNardi <matheuseduardonardi@gmail.com>
2021-10-13 20:39:08 -0300

    Adicionado Footer; Index concluída

--- pages/footer.php
+++ pages/footer.php
@@ -1,5 +1,20 @@
     </main>
     <!-- Footer -->
-    
+    <footer>
+        <p>© Copyright - Todos os Direitos Reservados</p>
+        <p>
+            <img src="../img/tel.png" alt="">
+            +55 19 99980-7002
+        </p>
+        <p>
+            <a href="">
+                <img src="../img/face.png" alt="">
+            </a>
+            &nbsp &nbsp &nbsp
+            <a href=""> 
+                <img src="../img/youtube.png" alt="">
+            </a>
+        </p>
+    </footer>
 </body>
 </html>
